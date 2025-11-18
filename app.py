7e4270e3fa92cef52a2069bc6c8bebebf3c26868
glendery <glensiregar1@gmail.com>
2025-11-18 11:47:00 +0700

    Initial commit: Setup basic Flask app

---
+++ app.py
@@ -0,0 +1,29 @@
+# app.py
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Halaman utama
+@app.route('/')
+def home():
+    return "Selamat datang di Toko E-commerce Sederhana!"
+
+# Halaman login (ini akan kita buat 'rentan' nanti)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # TODO: Proses login yang aman
+        username = request.form['username']
+        return f"Halo, {username}! (Login belum aman)"
+    
+    # Menampilkan form login
+    return """
+    <form method="post">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password"><br>
+        <input type="submit" value="Login">
+    </form>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
